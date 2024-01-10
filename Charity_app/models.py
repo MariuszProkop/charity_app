3042fa1e0cf260414bcfb57d8d706ace3d90abb0
mariusz <glodnymotyl@gmail.com>
2024-01-10 14:50:28 +0200

    initial commit

---
+++ Charity_app/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    TYPE_CHOICES = [
+        ('fundacja', 'Fundacja'),
+        ('organizacja_pozarzadowa', 'Organizacja pozarządowa'),
+        ('zbiorka_lokalna', 'Zbiórka lokalna'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES, default='fundacja')
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.PositiveIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=10)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'Donation {self.id}'
