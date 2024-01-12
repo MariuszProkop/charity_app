3fef810c3a05c2e0c4ba7c8cee3485ea1c122dc5
mariusz <glodnymotyl@gmail.com>
2024-01-12 16:02:41 +0200

    html3

--- Charity_app/views.py
+++ Charity_app/views.py
@@ -1,10 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import CreateView
+
+from Charity_app.models import Institution, Category, Donation
 
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        institutions = Institution.objects.all()
+
+        institution_count = Institution.objects.count()
+        total_bags = Donation.objects.aggregate(Sum('quantity'))['quantity__sum'] or 0
+        category = Category.objects.all()
+        return render(request, 'index.html', {'institutions': institutions, 'category': category,
+                                              'institution_count': institution_count, 'total_bags': total_bags})
 
 
 class AddDonationView(View):
@@ -17,9 +28,10 @@ class LoginView(View):
         return render(request, 'login.html')
 
 
-class RegisterView(View):
-    def get(self, request):
-        return render(request, 'register.html')
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = '/'
 
 class ConfirmationView(View):
     def get(self, request):
