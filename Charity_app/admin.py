b3a6b7f30de0bbbed6373a63e2332e2747dc5bf1
mariusz <glodnymotyl@gmail.com>
2024-01-14 17:39:56 +0200

    newcommit

--- Charity_app/admin.py
+++ Charity_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Category, Institution, Donation
 
+
+from .models import Category, Institution, Donation
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Institution)
 admin.site.register(Donation)
+
