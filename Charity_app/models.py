dc84f19fad125d6d24e2ede5ff95356b8a9c1c44
mariusz <glodnymotyl@gmail.com>
2024-01-14 21:08:09 +0200

    7

--- Charity_app/models.py
+++ Charity_app/models.py
@@ -45,28 +45,6 @@ class Donation(models.Model):
         return f'Donation {self.id}'
 
 
-from django.db import models
-from django.contrib.auth.models import User
 
-
-class Profile(AbstractUser):
-    name = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255)
-
-    @receiver(post_save, sender='auth.User')
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-
-    @receiver(post_save, sender='auth.User')
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
-
-    def __str__(self):
-        return self.email
-
-    class Meta:
-        verbose_name = 'Profile'
-        verbose_name_plural = 'Profiles'
 
 
--- Charity_app/views.py
+++ Charity_app/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView
 
-from Charity_app.forms import RegistrationForm
+
 from Charity_app.models import Institution, Category, Donation
 
 
@@ -16,10 +16,19 @@ class LandingPageView(View):
 
         institution_count = Institution.objects.count()
         total_bags = Donation.objects.aggregate(Sum('quantity'))['quantity__sum'] or 0
-        category = Category.objects.all()
-        return render(request, 'index.html', {'institutions': institutions, 'category': category,
-                                              'institution_count': institution_count, 'total_bags': total_bags})
 
+        fundacje = institutions.filter(type='fundacja')
+        zbiorki_lokalne = institutions.filter(type='zbiorka_lokalna')
+        organizacje = institutions.filter(type='organizacja_pozarzadowa')
+
+        return render(request, 'index.html', {
+            'institutions': institutions,
+
+            'institution_count': institution_count,
+            'total_bags': total_bags,
+            'fundacje': fundacje,
+            'zbiorki_lokalne': zbiorki_lokalne,
+            'organizacje': organizacje,})
 
 class AddDonationView(View):
     def get(self, request):
@@ -31,15 +40,9 @@ class LoginView(View):
         return render(request, 'login.html')
 
 
-class RegisterView(CreateView):
-    form_class = RegistrationForm
-    template_name = 'register.html'
-    success_url = reverse_lazy('index')
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        login(self.request, self.object)
-        return response
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
 
 
 class ConfirmationView(View):
